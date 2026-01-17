85fc20d259536cd846eb1a945aa6c2a4437cae2b
Jeremienomo <jeremienomo10@yahoo.com>
2026-01-17 00:01:36 -0800

    Add files via upload

---
+++ VarsAndMath25jan.java
@@ -0,0 +1,16 @@
+// VarsAndMath25jan.java
+// Variables, arithmetic, strings
+
+public class VarsAndMath25jan {
+    public static void main(String[] args) {
+
+        int num1 = 15;
+        int num2 = 20;
+
+        int sum = 0;
+
+        sum = num1 + num2;
+
+        System.out.println("\n sum is " + sum );
+    }
+}
