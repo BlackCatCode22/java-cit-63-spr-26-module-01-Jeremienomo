6eaccc2a5680d713b728b11a208f54f4b516d87e
Jeremienomo <jeremienomo10@yahoo.com>
2026-01-19 14:27:04 -0800

    Add files via upload

---
+++ FavoriteNumber.java
@@ -0,0 +1,20 @@
+// FavoriteNumber25jan.java
+// Variables, basic input/output, type handling.
+
+import java.util.Scanner;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your favorite number: ");
+
+        int favoriteNumber = scanner.nextInt();
+
+        System.out.println("Your favorite number is " + favoriteNumber + "! That's a great choice.");
+
+        scanner.close();
+    }
+}
+
